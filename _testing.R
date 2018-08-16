303a2b122acfba79f6cc6f21ccbb775b62ae0193
Sarah Schwartz <sarah.schwartz@usu.edu>
2018-08-15 21:36:45 -0600

    tidy up first draft

--- _testing.R
+++
@@ -1,7 +0,0 @@
-bookdown::render_book("index.Rmd", "bookdown::gitbook")
-bookdown::render_book("index.Rmd", "bookdown::pdf_book")
-
-
-
-bookdown::serve_book(dir = ".", output_dir = "_book", preview = TRUE, 
-           in_session = TRUE)
